fd25b8f7f004da8ba1aab84295711966fdbd732b
nharleytran <98291123+nharleytran@users.noreply.github.com>
2022-10-09 13:41:58 -0400

    Everything is good

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/hw3-meme-gen-starter/", // TODO update this!
+  base: "/hw3-meme-gen-nharleytran/",
   plugins: [react()]
 })
